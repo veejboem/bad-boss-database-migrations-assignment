09e08ad54e7546498b35b43b4be84697a29e2f75
Valerie Boem <vboem@gmail.com>
2017-02-02 15:41:35 -0500

    Renamed 'party_guests' table to 'widgets'.

---
+++ db/migrate/20170202203542_party_guests_to_widget.rb
@@ -0,0 +1,5 @@
+class PartyGuestsToWidget < ActiveRecord::Migration[5.0]
+  def change
+    rename_table :party_guests, :widgets
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202202326) do
+ActiveRecord::Schema.define(version: 20170202203542) do
 
   create_table "locations", force: :cascade do |t|
     t.string  "city"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20170202202326) do
     t.integer "quantity"
   end
 
-  create_table "party_guests", force: :cascade do |t|
+  create_table "widgets", force: :cascade do |t|
     t.string  "first_name"
     t.string  "last_name"
     t.string  "dietary_restrictions"
